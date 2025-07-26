58c6ec5f1eb666efbd0efd08a8546bc18335a8da
Antti Romppanen <romppanen.antti@gmail.com>
2025-07-26 21:41:29 +0300

    deleted useless types.ts

--- src/const/types.ts
+++
@@ -1,5 +0,0 @@
-export type IconThemeType = "numbers" | "icons";
-export type PlayerType = "player1" | "player2" | "player3" | "player4";
-
-/** Describes the grid size (4x4 or 6x6) */
-export type GridSizeType = 4 | 6;
